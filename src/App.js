f0a8269f3ccef3652be3720eec1eabdf1eafe346
Elizabeth <elizabeth@tealbook.com>
2019-03-21 10:31:38 -0400

    dud

--- src/App.js
+++ src/App.js
@@ -1,65 +1,27 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import CityCard from './CityCard';
-import JSONresults from './apiData/cities'
-//import SearchBar from './SearchBar';
-
-
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      city: '',
-      result: []
-    };
-  }
-
-  handleChange = (event) => {
-    this.setState({ city: event.target.value });
-  }
-
-
-  handleClick = (event) => {
-    event.preventDefault();
-    const result = JSONresults.filter(
-      city => {
-        return city.title.toLowerCase().startsWith(this.state.city.toLowerCase())
-      }
-    )
-    const newResult = this.state.result.filter(olderCity => olderCity.title !== result[0].title)
-    if (newResult.length > 4) {
-      newResult.pop()
-    }
-    this.setState({
-      result: [...result, ...newResult],
-      city: ''
-    })
-  }
-
-
   render() {
-    const results = this.state.result
-    console.log(results)
     return (
       <div className="App">
-        <form >
-          <label>
-            City:
-                <input type="text"
-              value={this.state.city}
-              onChange={this.handleChange}
-            />
-          </label>
-          <button onClick={this.handleClick}>
-            Submit
-            </button>
-        </form>
-        <CityCard
-          results={results}
-        />
-      </div>
 
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     );
   }
 }
