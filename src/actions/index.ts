1a9e105a8031ede9b44dbfd2c2aa60aab298cf43
ela-p-p <elizabeth.porter.pevec@gmail.com>
2019-04-02 15:03:19 -0400

    initial

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,23 +1,25 @@
 import * as constants from "../constants";
 import JSONresults from "../api/citiesData";
 
-// interface SetName {
-//     type: constants.SET_NAME
-// }
-// interface GetCity {
-//     type: constants.GET_CITY
-// }
+export interface SetName {
+    type: constants.SET_NAME;
+    payload?: string;
+}
+export interface GetCity {
+    type: constants.GET_CITY;
+    payload?: any
+}
 
-//export type cityAction = setName | getCity
+export type cityAction = SetName | GetCity;
 
-export const setName = name => {
+export function setName(name:string): SetName {
   return {
     type: constants.SET_NAME,
     payload: name
   };
 };
 
-export const getCity = name => {
+export function getCity(name:string): GetCity {
   return {
     type: constants.GET_CITY,
     payload: JSONresults.filter(city => {
--- src/constants/index.js
+++
@@ -1,5 +0,0 @@
-export const SET_NAME = "SET_NAME";
-//export type SET_NAME = typeof SET_NAME;
-
-export const GET_CITY = "GET_CITY";
-//export type GET_CITY = typeof GET_CITY;
